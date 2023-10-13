d5d5a6a41088eada8581333009ae04e25bca7434
Rangaz <ignacio2105@gmail.com>
2023-10-13 10:02:48 -0300

    r153 now only works outside celestial realities

--- src/core/secret-formula/achievements/normal-achievements.js
+++ src/core/secret-formula/achievements/normal-achievements.js
@@ -1745,8 +1745,8 @@ export const normalAchievements = [
     description: "Reality without producing antimatter.",
     checkRequirement: () => player.requirementChecks.reality.noAM,
     checkEvent: GAME_EVENT.REALITY_RESET_BEFORE,
-    get reward() { return `In non-celestial realities, gain a multiplier to EP based on how far 
-      away you are from obtaining ${formatPostBreak("1e4000")} EP.`},
+    get reward() { return `In non-celestial Realities, gain a multiplier to EP based on 
+      how far away you are from obtaining ${formatPostBreak("1e4000")} EP.`},
     effect: () => DC.E4000.divide(Currency.eternityPoints.value).pow(0.04).clampMin(10),
     effectCondition: () => !isInCelestialReality(),
     formatEffect: value => isInCelestialReality() ? `Inactive` : `${formatX(value, 2, 2)}`
