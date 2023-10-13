a390ea322bd00f2680797c7c26c3b75f1306842a
Rangaz <ignacio2105@gmail.com>
2023-10-13 11:19:21 -0300

    Fixed r65 expiration

--- src/core/secret-formula/changelog.js
+++ src/core/secret-formula/changelog.js
@@ -38,6 +38,7 @@ export const changelog = [
     <br>
     Post release changes:
     <ul>
+    <li>(13/10) Fixed Achievement 65's effect still becoming inactive after 3 minutes if you have Achievement 145.</li>
     <li>(13/10) Achievement 153's effect now only works in non-celestial Realities.</li>
     <li>(12/10) Cel-2's Reality is rebalanced:
     <ul>
