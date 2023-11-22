578b301e9e265afe03c63bffbcf0955d1267ad0d
Rangaz <ignacio2105@gmail.com>
2023-11-22 11:21:15 -0300

    Effarig & V are slightly harder

--- src/core/secret-formula/changelog.js
+++ src/core/secret-formula/changelog.js
@@ -32,10 +32,13 @@ export const changelog = [
     <li>Enhanced Achievement 53's bonus 8th antimatter dims now also use Replicanti and Tachyon Galaxies.</li>
     <li>Enhanced Achievement 61 is free.</li>
     <li>Enhanced Achievement 63 gives x8 the cost of an Infinity Dimension.</li>
-    <li>Enhanced Achievement 66 has a bigger multiplier (x16 -> x100).</li>
+    <li>Enhanced Achievement 66 has a bigger multiplier (x16 ➜ x100).</li>
     <li>Enhanced Achievement 78 is changed: It now makes you stat with 100 Time Theorems (goes up to 1e100 with
     Enhanced Achievement 55).</li>
-    <li>
+    <br>
+    <li>Teresa's Reality nerfs IP and EP more (^0.55 ➜ ^0.52).</li>
+    <li>Effarig's stages are slightly harder.</li>
+    <li>V's "Young Boy" Achievements have higher goals, but Perk Points used are twice as effective.
     `
   },
   {
