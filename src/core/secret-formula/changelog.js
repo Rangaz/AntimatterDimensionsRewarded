269732ed6715a58a733ad8346c8ffd6d68b7dd18
Rangaz <ignacio2105@gmail.com>
2023-11-23 18:52:48 -0300

    Balance check

--- src/core/secret-formula/changelog.js
+++ src/core/secret-formula/changelog.js
@@ -27,6 +27,7 @@ export const changelog = [
     <li>Enhanced Achievement 37 now also gives x5 Eternities.</li>
     <li>Enhanced Achievement 42 scales slightly better.</li>
     <li>Achievement 44 scales slightly better after 30 seconds.</li>
+    <li>Enhanced Achievement 46 is better.</li>
     <li>Achievements that rely on Antimatter Galaxies or total Galaxies (Er53, 66, 83, 132) are now based on
     best amount this Reality.</li>
     <li>Enhanced Achievement 53's bonus 8th antimatter dims now also use Replicanti and Tachyon Galaxies.</li>
