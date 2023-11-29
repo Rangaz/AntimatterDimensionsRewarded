8691253497da5001e42f72cd8a86faf277cd3e21
Rangaz <ignacio2105@gmail.com>
2023-11-29 13:17:36 -0300

    Fixed possible exploits on Pelle

--- src/core/celestials/pelle/pelle.js
+++ src/core/celestials/pelle/pelle.js
@@ -84,6 +84,7 @@ export const Pelle = {
     player.options.confirmations.glyphReplace = true;
     player.reality.automator.state.repeat = false;
     player.reality.automator.state.forceRestart = false;
+    player.reality.respecAchievements = true;
     if (BlackHoles.arePaused) BlackHoles.togglePause();
     player.celestials.pelle.doomed = true;
     Pelle.armageddon(false);
@@ -186,8 +187,8 @@ export const Pelle = {
   },
 
   get disabledAchievements() {
-    return [174, 172, 164, 162, 156, 153, 152, 143, 142, 141, 137, 136, 134, 133, 132, 126, 125, 124, 122, 121, 118, 117, 116, 115, 
-      113, 111, 108, 107, 106, 104, 103, 101, 97, 95, 93, 92, 91, 87, 85, 78, 77, 76, 74, 65, 62, 55, 54, 37, 33, 31];
+    return [174, 172, 164, 163, 162, 156, 153, 152, 143, 142, 141, 137, 136, 134, 133, 132, 126, 125, 124, 122, 121, 118, 117, 
+      116, 115, 113, 111, 108, 107, 106, 104, 103, 101, 97, 95, 93, 92, 91, 87, 85, 78, 77, 76, 74, 65, 62, 55, 54, 37, 33, 31];
   },
 
   get uselessInfinityUpgrades() {
